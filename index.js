ef7ab0d281251f02dfce83013cbc4c5b5e23ea56
techswarn <tech.swarns@gmail.com>
2024-03-06 17:09:14 +0530

    code update

--- index.js
+++ index.js
@@ -1,6 +1,19 @@
 const schedule = require("node-schedule");
-console.log("----------Starting Node---------schedular");
+const http = require("node:http");
+const hostname = "0.0.0.0";
+const port = 3000;
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader("Content-Type", "text/plain");
+  res.end("Schedular\n");
+});
+
+console.log("----------Starting Node schedular----------");
 
 const job = schedule.scheduleJob("* * * * *", function () {
   console.log("The answer to life, the universe, and everything!");
+});
+
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
 });
